8e1ff9db5bd4ae2d2a9236761a933f8f30e6e963
Larissa Pissurno <larissapissurno@gmail.com>
2024-07-03 10:43:10 -0300

    ex 3. extra 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,13 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={(event) => onNameChange(event.target.value)} />
+      <input id="name" value={name} onChange={(event) => setName(event.target.value)} />
     </div>
   )
 }
@@ -26,18 +28,17 @@ function FavoriteAnimal({animal, onAnimalChange}) {
 }
 
 
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   const [animal, setAnimal] = React.useState('')
-  const [name, setName] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={setName} />
+      <Name />
       <FavoriteAnimal animal={animal} onAnimalChange={setAnimal} />
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
